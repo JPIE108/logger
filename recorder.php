4518e010054b6d2fedf3376a5c91264a57c69e86
JPIE108 <39828710+JPIE108@users.noreply.github.com>
2018-06-01 21:06:16 +1000

    Create recorder.php

---
+++ recorder.php
@@ -0,0 +1,28 @@
+<html>
+
+ <head>
+
+     <title>
+Data Logger
+      </title> 
+       </head>
+
+
+
+
+   <body>
+   <p><h2>Welcome! Please Insert Test Data Below</h2></p>
+   
+    <div>
+   <form action="record.php" method="post">
+    <p><h4>Enter Some Data: </h4></p><input name="name" type="text">
+         <p><input type="submit" value="Submit Data!"></p>
+        </form>
+
+     </div>
+      </body>
+
+
+
+
+     </html>
